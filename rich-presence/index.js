012b7b9d5a9909dbfffd93435144e8f9f1cbe615
LuisFOsG <luisfosgdev@gmail.com>
2022-07-11 19:09:58 -0500

    feat: add rich presence

---
+++ rich-presence/index.js
@@ -0,0 +1,81 @@
+import dotenv from 'dotenv'
+import RPC from 'discord-rpc'
+
+import { DETAILS_LIST } from './listActivities.js'
+
+dotenv.config()
+const client = new RPC.Client({ transport: 'ipc' })
+
+const MINUTES = 50.5
+const ACTIVE_END = false
+const HOME_WORK = [0, 0]
+const DETAILS = DETAILS_LIST.testing
+const DEFAULT_STATUS = '🐺 Guardando Datos'
+
+const STATE = HOME_WORK[1] === 0 ? DEFAULT_STATUS
+: `Tareas Realizadas: ${HOME_WORK[0] === 0 ? `(0 de ${HOME_WORK[1]})` : ''}`
+
+const assets = {
+  large_image: DETAILS.image,
+  large_text: DETAILS.description,
+  small_image: 'awesome',
+  small_text: 'LuisFOsG',
+}
+
+const buttons = [
+  {
+    label: 'Visita mi GitHub',
+    url: "https://github.com/LuisFOsG"
+  },
+  {
+    label: 'Video Recomendado',
+    url: "https://youtu.be/XlgqZeeoOtI"
+  }
+]
+
+const timestamps = ACTIVE_END
+  ? {
+      start: Date.now(),
+      end: Date.now() + MINUTES * 60 * 1000
+    }
+  : {
+    start: Date.now()
+  }
+
+const party = HOME_WORK.includes(0)
+  ? {}
+  : {
+    id: 'LuisFOsG',
+    size: HOME_WORK,
+  }
+
+const ACTIVITY = {
+  state: STATE,
+  details: DETAILS.name,
+  assets,
+  party,
+  /* secrets: {
+    join: 'MTI4NzM0OjFpMmhuZToxMjMxMjM=',
+  }, */
+  buttons,
+  timestamps,
+  instance: true
+}
+
+client.on('ready', () => {
+  client.request('SET_ACTIVITY', {
+    pid: process.pid,
+    activity: ACTIVITY
+  })
+
+  console.log('============= Ready :D =============')
+  console.log('Authed for user: ', client.user.username);
+})
+
+client.on('error', () => {
+  console.log('Error :(')
+})
+
+client.login({
+  clientId: process.env.CLIENT_ID,
+})
---
+++ rich-presence/listActivities.js
@@ -0,0 +1,64 @@
+export const DETAILS_LIST = {
+  nodejs: {
+    name: '💻 Programando en NodeJS',
+    description: 'Pruebas.. xD',
+    image: 'image_1'
+  },
+  reactjs: {
+    name: '🏞 Desarrollando en ReactJS',
+    description: 'Pruebas.. xD',
+    image: 'image_2'
+  },
+  nextjs: {
+    name: '🚅 Desarrollando en NextJS',
+    description: 'Pruebas.. xD',
+    image: 'image_3'
+  },
+  testing: {
+    name: '🚧 Probando cosas',
+    description: 'Pruebas.. xD',
+    image: 'image_4'
+  },
+  configuring: {
+    name: '🤖 Configurando el proyecto',
+    description: 'Pruebas.. xD',
+    image: 'image_5'
+  },
+
+  reading: {
+    name: '📚 Leyendo',
+    description: 'Pruebas.. xD',
+    image: 'image_6'
+  },
+  netflix: {
+    name: '🤣 En Netflix',
+    description: 'Pruebas.. xD',
+    image: 'image_7'
+  },
+  playing: {
+    name: '🎮 Jugando',
+    description: 'Pruebas.. xD',
+    image: 'image_8'
+  },
+  music: {
+    name: '🎵 Escuchando música',
+    description: 'Pruebas.. xD',
+    image: 'image_9'
+  },
+
+  working: {
+    name: '🔥 En el trabajo',
+    description: 'Pruebas.. xD',
+    image: 'image_10'
+  },
+  reactjs_work: {
+    name: '🔥 Trabajando con ReactJS',
+    description: 'Pruebas.. xD',
+    image: 'image_2'
+  },
+  nodejs_work: {
+    name: '🔥 Trabajando con NodeJS',
+    description: 'Pruebas.. xD',
+    image: 'image_1'
+  }
+}
