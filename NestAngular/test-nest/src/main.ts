21f838ef0e72bf72cb7dc443a63ed487699daee2
LuisFOsG <luisfosgdev@gmail.com>
2022-12-27 12:14:30 -0500

    fix: cors

--- NestAngular/test-nest/src/main.ts
+++ NestAngular/test-nest/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 3000;
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  const PORT = process.env.PORT || 3000;
 
   app.enableCors();
-  await app.listen(PORT);
 
-  console.log(`Escuchando en el puerto ${PORT}`);
+  await app.listen(PORT, () => {
+    console.log(`🚀 Application is running on: http://localhost:${PORT}`);
+  });
 }
 
 bootstrap();
